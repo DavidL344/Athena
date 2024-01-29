6d4163f7348623f1d7d17ec3923d3ff8a41aa72d
David Langr <git@langr.tech>
2024-01-29 12:31:10 +0100

    Initial commit

---
+++ Athena/Program.cs
@@ -0,0 +1,11 @@
+﻿using Cocona;
+
+var builder = CoconaApp.CreateBuilder(args, x =>
+{
+    x.EnableShellCompletionSupport = true;
+    x.TreatPublicMethodsAsCommands = false;
+});
+
+var app = builder.Build();
+
+app.Run();
