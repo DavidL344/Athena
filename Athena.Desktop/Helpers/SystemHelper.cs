ff226212d4301cfc905068be059afb2c3a00f5de
David Langr <git@langr.tech>
2024-09-19 15:16:28 +0200

    Dynamically fetch the file extension on Windows
    Applications and scripts on Windows can be called without specifying their file extension, which won't pass the app's checks without a proper lookup

--- Athena.Desktop/Helpers/SystemHelper.cs
+++ Athena.Desktop/Helpers/SystemHelper.cs
@@ -4,9 +4,25 @@ public class SystemHelper
 {
     public static string WhereIs(string command)
     {
-        var paths = Environment.GetEnvironmentVariable("PATH")?.Split(Path.PathSeparator);
+        var paths = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Machine)?.Split(Path.PathSeparator);
         if (paths is null)
             throw new ApplicationException("Environment variable 'PATH' not found!");
+        
+        string[] executableFileExtensions = [".exe", ".bat", ".cmd", ".ps1"];
+        if (OperatingSystem.IsWindows() && !EndsWithAtLeastOne(command, executableFileExtensions))
+        {
+            foreach (var fileExtension in executableFileExtensions)
+            {
+                foreach (var path in paths)
+                {
+                    var fullPath = Path.Combine(path, $"{command}{fileExtension}");
+                    if (File.Exists(fullPath))
+                        return fullPath;
+                }
+            }
+            
+            return string.Empty;
+        }
         
         foreach (var path in paths)
         {
@@ -34,4 +50,7 @@ public class SystemHelper
         
         return editors.Last();
     }
+
+    private static bool EndsWithAtLeastOne(string @string, IEnumerable<string> anyOccurence)
+        => anyOccurence.Any(@string.EndsWith);
 }
