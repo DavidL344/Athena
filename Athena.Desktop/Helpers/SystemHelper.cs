0cd2c0e5cf15d2cbd1b611143c9a4d26da20b602
David Langr <git@langr.tech>
2024-09-20 20:24:12 +0200

    Fix a crash caused by an incompatible PATH target
    The targets are platform-specific, meaning a different one is needed

--- Athena.Desktop/Helpers/SystemHelper.cs
+++ Athena.Desktop/Helpers/SystemHelper.cs
@@ -4,7 +4,10 @@ public class SystemHelper
 {
     public static string WhereIs(string command)
     {
-        var paths = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Process)?.Split(Path.PathSeparator);
+        var paths = OperatingSystem.IsWindows()
+            ? Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Process)?.Split(Path.PathSeparator)
+            : Environment.GetEnvironmentVariable("PATH")?.Split(Path.PathSeparator);
+        
         if (paths is null)
             throw new ApplicationException("Environment variable 'PATH' not found!");
         
